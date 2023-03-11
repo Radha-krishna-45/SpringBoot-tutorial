20f3ef41acb262800b22ea9fc173370fa136bd77
radha-krishna-45 <radhakrishna4956@gmail.com>
2023-03-11 15:59:55 -0500

    SBD-1 Created exception handling in the controller methods Added error message class Changed the method FetchbyID() and added the custom exception

--- src/main/java/com/Springboot/Controller/DepartmentController.java
+++ src/main/java/com/Springboot/Controller/DepartmentController.java
@@ -1,2 +1,58 @@
-package com.Springboot.Controller;public class DepartmentController {
+package com.Springboot.Controller;
+
+
+import com.Springboot.Entity.Department;
+import com.Springboot.Service.DepartmentService;
+import com.Springboot.Service.DepartmentServiceIImplementation;
+import jakarta.validation.Valid;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+public class DepartmentController {
+
+    @Autowired
+    private DepartmentService departmentService;
+
+    private final Logger logger = LoggerFactory.getLogger(DepartmentController.class);
+
+    @PostMapping("/departments")
+    public Department saveDepartment(@Valid @RequestBody Department department) {
+        logger.info("we hit the post request. Data is saved in the records");
+        return departmentService.saveDepartment(department);
+    }
+
+    @GetMapping("/departments")
+    public List<Department> getDepartmentsList() {
+        logger.info("get request hit, so we get the data");
+        return departmentService.getDepartments();
+    }
+
+    @GetMapping("/departments/{id}")
+    public Department getDepartmentById(@PathVariable("id") Long departmentId) throws Exception {
+        return departmentService.getDepartmentsbyID(departmentId);
+    }
+
+    @DeleteMapping("/departments/{id}")
+    public String deleteByID(@PathVariable("id") Long departmentId) {
+        departmentService.deleteRecords(departmentId);
+        return "Department deleted successfully";
+    }
+
+    @PutMapping("/departments/{id}")
+    public Department updateDepartment(@PathVariable("id") Long departmentId, @RequestBody Department department) {
+        return departmentService.UpdateDepartment(departmentId, department);
+
+    }
+
+    @GetMapping("/departments/departmentName/{name}")
+    public Department departmentDataByName(@PathVariable("name") String departmentName) {
+        return departmentService.getDataByName(departmentName);
+
+    }
+
 }
--- src/main/java/com/Springboot/Entity/Department.java
+++ src/main/java/com/Springboot/Entity/Department.java
@@ -1,2 +1,31 @@
-package com.Springboot.Entity;public class Department {
+package com.Springboot.Entity;
+
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.validator.constraints.Length;
+
+@Entity
+@Data
+@NoArgsConstructor
+public class Department {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+    private String company;
+    @NotBlank(message = "Please add the name of the department")
+    @Length(max = 7, min = 0)
+    private String departmentName;
+
+    private String address;
 }
+
+
+
+
